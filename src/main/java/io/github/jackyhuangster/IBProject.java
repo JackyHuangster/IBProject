665b3d3ac2588a811a1c9ac6178006cc7da5d909
Jacky <jh123892@gmail.com>
2017-12-01 21:17:39 -0800

    Main Screen

--- src/main/java/io/github/jackyhuangster/IBProject.java
+++ src/main/java/io/github/jackyhuangster/IBProject.java
@@ -1,7 +1,12 @@
 package io.github.jackyhuangster;
 
+import javax.swing.*;
+
 public class IBProject {
     public static void main(String[] args ) {
         System.out.println("Eyyy");
+        JFrame window = new JFrame("IBProject");
+        window.setVisible(true);
+
     }
 }
