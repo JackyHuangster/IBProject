4d7d9d81a0f38f01b21698e29b5f49b3e1d77e79
Jacky <jh123892@gmail.com>
2017-12-27 17:54:48 -0800

    Main Screen

--- src/main/java/io/github/jackyhuangster/IBProject.java
+++ src/main/java/io/github/jackyhuangster/IBProject.java
@@ -4,9 +4,29 @@ import javax.swing.*;
 
 public class IBProject {
     public static void main(String[] args ) {
-        System.out.println("Eyyy");
         JFrame window = new JFrame("IBProject");
+        window.setSize(1280, 720);
         window.setVisible(true);
+        Icon icon = new ImageIcon(IBProject.class.getResource("SVT.gif"));
+        Icon icontwo = new ImageIcon(IBProject.class.getResource("SVT.gif"));
+        JLabel loadingImage = new JLabel(icon);
+        loadingImage.setSize(1280, 720);
+        //window.add(loadingImage);
+        window.add(loadingImage, 0);
+        window.setVisible(true);
+        try{
+            Thread.sleep(5000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        loadingImage.setVisible(false);
+        window.setVisible(false);
+        JLabel loadingImageTwo = new JLabel(icontwo);
+        loadingImageTwo.setSize(1280, 720);
+        //window.add(loadingImageTwo);
+        window.add(loadingImageTwo, 0);
+        window.setVisible(true);
+
 
     }
 }
